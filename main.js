542c0686bbc95118c560da7953a42d973c840f3a
Tatiana <tat2024ka@gmail.com>
2025-08-17 00:44:15 +0300

    feat: add page profile and page fight

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ document.addEventListener("DOMContentLoaded", function () {
   const settingPage = document.querySelector(".setting");
   const profilePage = document.querySelector(".profile");
   const topSection = document.querySelector(".top-section");
+  const fightPage = document.querySelector(".fight");
 
   // Buttons
   const btnCreateCharacter = document.querySelector(".btn-create-character");
@@ -12,7 +13,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const btnHome = document.querySelector(".icon-home");
   const btnProfile = document.querySelector(".icon-profile");
   const btnSetting = document.querySelector(".icon-setting");
-  let textTopSection = document.querySelector(".main-txt");
+  const textTopSection = document.querySelector(".main-txt");
+  const btnFight = document.querySelector(".btn-fight");
 
   //Переход настраницу настроек
   btnSetting.addEventListener("click", () => {
@@ -42,6 +44,16 @@ document.addEventListener("DOMContentLoaded", function () {
     profilePage.style.display = "flex";
     topSection.style.display = "flex";
     textTopSection.textContent = "Profile";
+  });
+
+  // Переход на страницу боя
+  btnFight.addEventListener("click", () => {
+    pages.forEach((page) => {
+      page.style.display = "none";
+    });
+    fightPage.style.display = "flex";
+    topSection.style.display = "flex";
+    textTopSection.textContent = "Fight";
   });
 
   // Тут хранится переменная имени
@@ -101,6 +113,145 @@ document.addEventListener("DOMContentLoaded", function () {
     inputSetting.classList.toggle("active");
     savedName = document.querySelector(".edit-name-input").value;
     localStorage.setItem("name", savedName);
+
     textSetting.textContent = `Player Name: ${savedName}`;
   });
+
+  console.log(savedName);
+  // Сраница смны персонажа
+  // Переписываю имя персонажа
+  const textProfile = document.querySelector(".text-profile");
+  textProfile.textContent = `Your name: ${savedName}`;
+  // Coxрфняю в переменные побед и проигрышей
+  let winsValue = document.querySelector(".wins-value");
+  let losesValue = document.querySelector(".loses-value");
+
+  // current character
+  const btnMainCharacter = document.querySelector(".button-change-character");
+  // Block with chang character
+  const changeCharacter = document.querySelector(".change-character");
+  // Click current character
+  btnMainCharacter.addEventListener("click", () => {
+    changeCharacter.style.display = "flex";
+    document.querySelector(".overlay-big").style.display = "block";
+  });
+
+  // При клике вне блока с доп персонажами закрываем блок персонажей
+  document.addEventListener("mouseup", function (e) {
+    if (!changeCharacter.contains(e.target)) {
+      changeCharacter.style.display = "none";
+      document.querySelector(".overlay-big").style.display = "none";
+    }
+  });
+
+  // Блок с картинкой главного персонажа
+  const currentCharacter = document.querySelector(".main-character");
+  // Переменная с локал сторедж источника
+  let currentCharacterSrc = localStorage.getItem("character");
+
+  //Собираю в коллекцию кнопки доп персонажей
+  const btnExtraCharacter = document.querySelectorAll(".choose-character");
+
+  // Контейнер сдля картинки персонажа на странице боя
+  const myCharacter = document.getElementById("my-character-container");
+
+  // Первоначальное создание картинки на странице боя
+  const myCharacterCreateImage = document.createElement("img");
+  myCharacterCreateImage.src = `${currentCharacterSrc}`;
+  myCharacter.appendChild(myCharacterCreateImage);
+
+  // Прохожусь циклом по копкам выбора и при нажатии на какую то меняем местами текущего персонажа и дополнительного
+  btnExtraCharacter.forEach((btn, index) => {
+    btn.addEventListener("click", () => {
+      // Коллекция картинок с доп персонажами
+      let extraCharacters = document.querySelectorAll(".extra-character");
+
+      // Массив с источниками картинок оп персонажей
+      const arraySrc = [];
+      for (let i = 0; i < extraCharacters.length; i++) {
+        arraySrc.push(extraCharacters[i].src);
+      }
+
+      // Созраняем источник первоначальной картинки
+      let a = currentCharacter.src;
+
+      // Заменяем источник изначальной картинки на тот который был кликнут
+      currentCharacter.src = `${arraySrc[index]}`;
+
+      // Сохраняем в локал источник той картинки котораятеперь встала на место основной
+      localStorage.setItem("character", currentCharacter.src);
+
+      // Получем источник теперь главной картинки
+      currentCharacterSrc = localStorage.getItem("character");
+
+      // Меняем источник кликнутой картинки на изначально основной
+      extraCharacters[index].src = `${a}`;
+
+      document.querySelector(".overlay-big").style.display = "none";
+      changeCharacter.style.display = "none";
+
+      // Меняем источник созданной картинки на стрранице боя
+      changeCharacterinFight();
+    });
+  });
+
+  // БОЙ
+  // Функция которая меняет персонажа на страние боя в момент выбора персонажа на странице настроек
+  function changeCharacterinFight() {
+    myCharacterCreateImage.src = `${currentCharacterSrc}`;
+  }
+
+  // Тут вставляем имя персонажа сохраненное ранее
+  const nameFight = document.querySelector(".name-fight");
+  nameFight.textContent = `${savedName}`;
+
+  const penguin = {
+    name: "Сute penguin",
+    alive: "120",
+    src: "./assets/main-character/пингвин.jpg",
+    alt: "Opponent penguin",
+  };
+
+  const deer = {
+    name: "Terrible deer",
+    alive: "100",
+    src: "./assets/main-character/олень.jpg",
+    alt: "Opponent deer",
+  };
+
+  const snowMaiden = {
+    name: "Snow Maiden",
+    alive: "150",
+    src: "./assets/main-character/снегурка.jpg",
+    alt: "Opponent snow maiden",
+  };
+
+  const arrayOfOpponents = [penguin, deer, snowMaiden];
+  // Перешиваем массив
+  function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array;
+  }
+
+  /*   shuffleArray(arrayOfOpponents); */
+
+  const nameOpponent = document.querySelector(".name-opponent");
+  const currentAliveOpponent = document.querySelector(
+    ".opponent-current-alive"
+  );
+  const allAliveOpponent = document.querySelector(".all-alive-opponent");
+  currentAliveOpponent.textContent = arrayOfOpponents[0].alive;
+  allAliveOpponent.textContent = arrayOfOpponents[0].alive;
+
+  // Сoздаю первого персонажа
+  const containerOpponent = document.getElementById("opponent-container");
+  const firstOpponent = document.createElement("img");
+  firstOpponent.src = arrayOfOpponents[0].src;
+  firstOpponent.alt = arrayOfOpponents[0].alt;
+  containerOpponent.appendChild(firstOpponent);
+  nameOpponent.textContent = arrayOfOpponents[0].name;
+  console.log(firstOpponent.src);
 });
